76d3c76a5ceef67f25825fa29739f5639f38b5d9
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-07 11:02:19 -0700

    seeds db with products, reviews

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'faker', github: 'stympy/faker'
 
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Product.destroy_all
+
+50.times do |index|
+  product = Product.create!(
+    name: Faker::Lorem.word + " fish",
+    date_caught: Faker::Date.between(8.days.ago, Date.today),
+    origin: Faker::LordOfTheRings.location,
+    price: Faker::Number.decimal(2, 2)
+  )
+  Faker::Number.between(0, 7).times do |index|
+    product.reviews.create!(
+      comment: Faker::TwinPeaks.quote,
+      rating: Faker::Number.between(1, 5),
+    )
+  end
+end
